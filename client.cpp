066743b3d0e412471adcfa7e637b8fcc2858c042
Wajeeha Tahir <88159584+WajeehaTahir@users.noreply.github.com>
2023-08-26 22:02:05 +0500

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,273 @@
+#include <iostream>
+#include <thread>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <fstream>
+
+#define SERVER_PORT_NO 80
+
+using namespace std;
+
+int joinConnection(string, int);
+int makeConnection(int, string);
+void getFile(int, int);
+void output(int);
+void input(int);
+
+bool chat_flag = false;		//flag to pause chat with server
+int wait_flag = 1;	//flag to wait for a response from user
+
+void getFile(int connfd, int connfd_server)
+{	
+	cout << "Chat with peer? y/n, e for exit" << endl;
+	while(wait_flag == 1);	//wait for response
+	
+	if(wait_flag == 2)	//if yes
+	{
+		thread out(output,connfd);
+		thread in(input,connfd);
+		in.join();	
+	  	out.join();
+  	}
+  	
+  	cout << "Chat closed, file is being downloaded" << endl;
+	
+	char buffer[100];
+	bzero(buffer, 100);
+	recv(connfd, buffer, 100, 0);
+	string filename = buffer;
+	ofstream file("files/" + filename);	//create empty file
+	while(1)
+	{
+		bzero(buffer, 100);
+		recv(connfd, buffer, 100, 0);
+		if(!strcmp(buffer, "\n\r\n"))	//if end sequence, end
+		{
+			break;
+		}
+		file << buffer;	//write buffer to file
+	}
+	file.close();
+	
+	cout << "File download complete, closing connection" << endl;
+	
+	send(connfd, "e", strlen("e"), 0);	//notify seeder to end connection
+  	close(connfd);
+  	
+  	send(connfd_server, "files\n", strlen("files\n"), 0);	//add downloaded file to server database
+  	sleep(0.5);
+  	send(connfd_server, (filename + "\n").c_str(), strlen((filename + "\n").c_str()), 0);
+  	sleep(0.5);
+  	send(connfd_server, "\n\r\n", strlen("\n\r\n"), 0);
+}
+int joinConnection(string ip = "127.0.0.1", int port = 80)
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		perror("Socket Creation failed\n");
+		return -1;
+	}
+
+	struct sockaddr_in s_addr;
+
+	s_addr.sin_family 	= AF_INET;
+	s_addr.sin_port	= htons(port);
+	inet_aton(ip.c_str(), &s_addr.sin_addr);
+	
+	if (connect(fd, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+		perror("Socket Connect failed\n");
+		return -1;
+	}
+	
+	return fd;
+}
+int makeConnection(int connfd_server, string filename)
+{
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		perror("Socket Creation failed\n");
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+
+	addr.sin_addr.s_addr	= INADDR_ANY;
+	addr.sin_family	= AF_INET;
+	addr.sin_port		= htons(0);	//zero chooses a free port
+
+	if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
+		perror("Bind failed on socket\n");
+		return -1;	
+	}
+
+	int backlog = 1;
+	if (listen(fd, backlog) == -1) {
+		perror("Listen Failed on server: \n");
+		return -1;
+	}
+	
+	struct sockaddr_in cliaddr;
+	socklen_t cliaddr_len = sizeof cliaddr;
+	
+	socklen_t sin_addr_len = sizeof addr;
+	getsockname(fd, (struct sockaddr *)&addr, &sin_addr_len);
+	
+	cout << "File download request received, listening for connection on port: " << ntohs(addr.sin_port) << endl;
+	string new_port = to_string(ntohs(addr.sin_port));	//port number of newly created port
+	
+	send(connfd_server, "port\n", strlen("port\n"), 0);
+	sleep(1);
+	send(connfd_server, new_port.c_str(), strlen(new_port.c_str()), 0);	//send port number to server
+	
+	int connfd = accept(fd, (struct sockaddr *) &cliaddr, &cliaddr_len);
+	if (connfd <=0 ) {
+		perror("accept failed on socket: ");
+	}
+	cout << "Peer connected" << endl;
+	cout << "Chat with peer? y/n, e for exit" << endl;
+	
+	while(wait_flag == 1);	//wait for response
+	
+	if(wait_flag == 2)	//if yes, create chat threads
+	{
+		thread out(output,connfd);
+		thread in(input,connfd);
+		
+		in.join();
+		out.join();
+	}
+  	
+  	cout << "Closing chat, file is being sent\n" << endl;
+  	sleep(1);
+	
+	send(connfd, filename.c_str(), strlen(filename.c_str()), 0);	//send filename
+	sleep(1);
+	
+	ifstream file("files/" + filename);
+	string text;
+	
+	while(getline(file, text))
+	{
+		send(connfd, (text + "\n").c_str(), strlen((text + "\n").c_str()), 0);	//send file contents
+	}
+	
+	sleep(1);
+	send(connfd, "\n\r\n", strlen("\n\r\n"), 0);	//send end sequence
+	
+	char buffer[1];
+	bzero(buffer, 1);
+	recv(connfd, buffer, 1, 0);	//wait for "e" from leecher
+  	
+  	close(connfd);
+  	cout << "Connection closed with peer" << endl;
+
+	return 1;
+}
+void input(int connfd)
+{
+	while(1)
+	{
+		char buffer[1024];
+		bzero(buffer, 1024);
+		recv(connfd, buffer, 1024, 0);
+		string b = buffer;
+		
+		if(!strcmp(buffer,"e")) 	//if server ends connection
+		{
+			break;
+		}
+		else if(b.substr(0, 7) == "seeder\n")	//if server sends seeder information
+		{
+			string ip = b.substr(7, b.find("\t"));
+			string port = b.substr(b.find("\t") + 1, strlen(b.c_str()));
+			chat_flag = true;	//pause chat with server
+			int fd = joinConnection(ip, stoi(port));
+			thread downloader(getFile, fd, connfd);
+			downloader.join();
+			chat_flag = false;	//resume chat with server
+		}
+		else if (b.substr(0, 5) == "seed\n")	//if server forwards file request
+		{
+			string filename = b.substr(5, strlen(b.c_str()));
+			chat_flag = true;
+			thread connect(makeConnection, connfd, filename);
+			connect.join();
+			chat_flag = false;
+		}
+		else
+		{
+			cout << buffer << endl;
+		}
+	}
+}
+void output(int connfd)
+{
+	while(1)
+	{
+		char buffer[100];
+		bzero(buffer, 100);
+		cin.getline(buffer, 100);
+		if(!strcmp(buffer,"y") || !strcmp(buffer,"n") )
+		{
+			if(!strcmp(buffer,"y"))
+			{
+				wait_flag = 2;	//flag to create chat
+			}
+			else
+			{
+				wait_flag = 0;
+			}
+			while(chat_flag);	//pause thread until chat_flag is false
+			wait_flag = true;
+		}
+		else
+		{
+			send(connfd, buffer, strlen(buffer), 0);
+
+			if(!strcmp(buffer,"e"))	//e for exit
+			{
+				break;
+			}
+		}
+	}
+}
+
+int main() {
+
+	int fd = joinConnection();
+	
+	if (fd > 0)
+	{
+		send(fd, "files\n", sizeof("files\n"), 0);
+		
+		DIR *dp;
+		struct dirent *dirp;
+		dp = opendir("./files");
+		
+		while((dirp = readdir(dp)) != NULL)
+		{
+			string filename = dirp->d_name;
+			if(filename[0] != '.')
+			{
+				send(fd, filename.c_str(), sizeof(filename), 0);	//send filenames
+			}
+		}
+		
+		send(fd, "\n\r\n", sizeof("\n\r\n"), 0);	//send end sequence
+		closedir(dp);
+		
+		thread out(output,fd);	//threads to chat with server
+		thread in(input,fd);
+		out.join();
+	  	in.join();
+
+	    	close(fd);
+    	}
+	return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <thread>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <list>
+#include <map>
+
+#define SERVER_PORT_NO 80
+
+using namespace std;
+
+struct client
+{
+	string ip;
+	int port;
+	int serial;
+	int fd;
+	client(string i, int p, int s, int f)
+	{
+		ip = i;
+		port = p;
+		serial = s;
+		fd = f;
+	}
+	bool operator ==(const client& obj)	//overloading operators for list operations
+	{
+		return obj.serial == serial;
+	}
+	bool operator !=(const client& obj)
+	{
+		return obj.serial != serial;
+	}
+};
+
+list <client> clients;
+multimap <string, int> files;	//filenames as key, serial numbers as values
+
+void input(client obj)
+{
+	int connfd = obj.fd;
+	while(1)
+	{
+		char buffer[100];
+		bzero(buffer, 100);
+		recv(connfd, buffer, 100, 0);
+		
+		if(!strcmp(buffer, "e")) 
+		{	
+			send(connfd, "e", strlen("e"), 0);
+			clients.remove(obj);	//remove client from list
+			
+			for(multimap<string, int>::iterator i = files.begin(); i!= files.end();)
+			{
+				if(i->second == obj.serial)
+				{
+				 	i = files.erase(i);	//remove files for that client
+				}
+				else
+				{
+					++i;
+				}
+			}
+			
+			string message = "Client " + to_string(obj.serial) + " has left";
+			cout << endl << message << endl;
+			cout << "Clients currently connected: " << endl;
+			if(clients.size() > 0)
+			{
+				for (const auto&c:clients)
+				{
+					cout << to_string(c.serial) + "- IP: " + c.ip + ", Port: "+ to_string(c.port) << endl;
+				}
+			}
+			else
+			{
+				cout << "None" << endl;
+			}
+			for (const auto&c:clients)
+			{
+				send(c.fd, message.c_str(), strlen(message.c_str()), 0);	//notify other clients
+			}
+			close(connfd);
+			break;
+		}
+		else if(!strcmp(buffer, "files\n"))
+		{
+			cout << "Files: " << endl;
+			while(1)
+			{
+				bzero(buffer, 100);
+				recv(connfd, buffer, 100, 0);
+				if (!strcmp(buffer, "\n\r\n"))	//if end sequence, break
+				{
+					break;
+				}
+				files.insert(pair<string, int>(buffer, obj.serial));	//store filenames with serial number
+				cout << buffer << endl;
+			}
+			cout << endl;
+		}
+		else if (!strcmp(buffer, "get"))
+		{
+			for (const auto&f:files)
+			{
+				if(f.second != obj.serial)
+				{
+					string message = f.first + " at client " + to_string(f.second) + "\n";
+					send(connfd, message.c_str(), strlen(message.c_str()), 0);	//send filenames and client serial
+				}
+			}
+			send(connfd, "\nEnter \"none\" to go back", strlen("\nEnter \"none\" to go back"), 0);
+			bzero(buffer, 100);
+			recv(connfd, buffer, 100, 0);
+			string filename = buffer;
+			
+			int serial = -1;
+			for(auto i = files.begin(); i!= files.end(); i++)
+			{
+				if(i->first == buffer)
+				{
+					if(i->second != obj.serial)
+					{
+						serial = i->second;	//get serial number of client with that file (seeder)
+					}
+				}
+			}
+			if (serial > 0)	//if filename was not found, serial will be -1
+			{
+				string port;
+				string ip;
+				int connfd_seeder;
+				
+				for(auto i = clients.begin(); i!= clients.end(); i++)
+				{
+					if (i->serial == serial)
+					{
+						ip = i->ip;	//get ip address and fd of seeder
+						connfd_seeder = i->fd;
+					}
+				}
+				
+				send(connfd_seeder, ("seed\n" + filename).c_str(), strlen(("seed\n" + filename).c_str()), 0);	//notify seeder to create connection
+				
+				bzero(buffer, 100);
+				recv(connfd_seeder, buffer, 100, 0);
+				
+				if (!strcmp(buffer, "port\n"))
+				{
+					recv(connfd_seeder, buffer, 100, 0);
+				}
+				
+				port = buffer;
+				send(connfd, ("seeder\n" + ip + "\t" + port).c_str(), strlen(("seeder\n" + ip + "\t" + port).c_str()), 0); //send port number and ip address of seeder to leecher
+			}
+			
+		}
+		else if(!strcmp(buffer, "port\n"))	
+		{
+			sleep(3);	//pause chat for 3 seconds
+		}
+	}
+}
+
+int main() {
+	
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1) {
+		perror("Socket Creation failed\n");
+		return -1;
+	}
+
+	struct sockaddr_in addr;
+
+	addr.sin_addr.s_addr	= INADDR_ANY;
+	addr.sin_family	= AF_INET;
+	addr.sin_port		= htons(SERVER_PORT_NO);
+
+	if (bind(fd, (struct sockaddr*) &addr, sizeof(addr)) == -1) {
+		perror("Bind failed on socket\n");
+		return -1;	
+	}
+
+	int backlog = 1;
+	if (listen(fd, backlog) == -1) {
+		perror("Listen Failed on server: \n");
+		return -1;
+	}
+
+	int count = 0;
+	int connfd;
+	
+	struct sockaddr_in cliaddr;
+	socklen_t cliaddr_len = sizeof cliaddr;
+	
+	while(1)
+	{
+		connfd = accept(fd, (struct sockaddr *) &cliaddr, &cliaddr_len);
+		if (connfd <=0 ) {
+			perror("accept failed on socket: ");
+		}
+		
+		count++;
+		client obj(string(inet_ntoa(cliaddr.sin_addr)), int(ntohs(cliaddr.sin_port)), count, connfd);
+		clients.push_back(obj);	//push client object to list
+		cout << count << "- IP: " << obj.ip;
+		cout << ", Port: " << obj.port << endl; 
+		
+		string buffer = "Client " + to_string(obj.serial) + " with IP " + string(obj.ip) + " at port " + to_string(obj.port) + " has joined";
+		for (const auto&c:clients)
+		{
+			if(c.serial == obj.serial)
+			{
+				send(obj.fd, "Welcome to the server", strlen("Welcome to the server"), 0);
+			} 
+			else
+			{
+				send(c.fd, buffer.c_str(), strlen(buffer.c_str()), 0);	//notify existing clients
+			}
+		}
+		
+		send(connfd, "\nUsers already connected:", strlen("\nUsers already connected:"), 0);
+		if(count > 1)
+		{
+			for (const auto&c:clients)
+			{
+				if(c.serial != obj.serial)
+				{
+					string buff = "\n" + to_string(c.serial) + "- " + c.ip + ", "+ to_string(c.port);
+					send(connfd, buff.c_str(), strlen(buff.c_str()), 0);
+				}
+			}
+		}
+		else	//if there are no other users
+		{
+			send(connfd, "\nNone", strlen("\nNone"), 0);
+		}
+		thread in(input,obj);
+		in.detach();
+	}
+	return 0;
+}
